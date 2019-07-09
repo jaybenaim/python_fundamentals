3f1c2afe3c4ca152b9493dda80a63bf99ebe3fc9
jaybenaim <benaimjacob@gmail.com>
2019-07-09 13:42:30 -0400

    exercise2 second challenge complete

--- exercises2.py
+++ exercises2.py
@@ -1,6 +1,14 @@
-def total_tip(): 
-    tip = 55
-    tax = 55 * .15
-    print(tip + tax )
+# def total_tip(): 
+#     tip = 55
+#     tax = 55 * .15
+#     print(tip + tax )
 
-total_tip() 
+# total_tip() 
+
+def integar_concat(): 
+    text = "4"
+    num = 3
+    con = str(text) + str(num) 
+    print(con)
+    
+integar_concat() 
