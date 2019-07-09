ebd9790be05a2e4db212b7e2822955d461a3ec02
jaybenaim <benaimjacob@gmail.com>
2019-07-09 13:51:09 -0400

    exercise 3

---
+++ exercises3.py
@@ -0,0 +1,18 @@
+print("what is your name?")
+
+def print_hello(): 
+    ask = input("What is your name?\n").upper()
+    ask
+    a = "Hi "
+    
+def variables(): 
+    age = 25
+    var = "My age is {}".format(24)
+    new_age = "Data is {}".format(type(age))
+    print(var)
+    print(new_age)
+    
+
+print_hello()
+variables() 
+
