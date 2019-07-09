22cd3a8c5175a87691fe0e2347e4f2a43d6e82de
jaybenaim <benaimjacob@gmail.com>
2019-07-09 13:56:19 -0400

    exercises3 up to cookies

--- exercises3.py
+++ exercises3.py
@@ -1,18 +1,42 @@
-print("what is your name?")
+# print("what is your name?")
 
-def print_hello(): 
-    ask = input("What is your name?\n").upper()
-    ask
-    a = "Hi "
+# def print_hello(): 
+#     ask = input("What is your name?\n").upper()
+#     ask
+#     a = "Hi "
+
+# def variables(): 
+#     age = 25
+#     var = "My age is {}".format(24)
+#     new_age = "Data is {}".format(type(age))
+#     print(var)
+#     print(new_age)
     
-def variables(): 
-    age = 25
-    var = "My age is {}".format(24)
-    new_age = "Data is {}".format(type(age))
-    print(var)
-    print(new_age)
-    
 
-print_hello()
-variables() 
+# # print_hello()
+# # variables() 
 
+# def new_user(): 
+#     user_name = input() 
+#     print("Hello, {}".format(user_name))
+
+# new_user()
+
+# def secret_pass(): 
+#     secret_password = "jay"
+
+#     print("What's the password?")
+#     password_attempt = input()
+
+#     correct_or_not = (password_attempt == secret_password)
+
+#     print("That's {}!".format(correct_or_not))
+# secret_pass()
+
+# def cookies(): 
+#     print("How many cookies have been eaten?")
+#     eaten = input()
+#     # convert user input to integer and subtract it from 12
+#     remaining_cookies = 12 - int(eaten)
+#     print("There are {} cookies left.".format(remaining_cookies))
+# cookies() 
