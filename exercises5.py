cb3f8681af4e8b8f0e241399814bda64ddb7b5b4
jaybenaim <benaimjacob@gmail.com>
2019-07-10 10:15:16 -0400

    added rest choice

--- exercises5.py
+++ exercises5.py
@@ -3,8 +3,9 @@ distance = 0
 energy = 10
 eat = (energy + 2) 
 spacer = "\n------------------------------------------------------------------------------ \n"
+rest = (energy + 3) 
 
-print("Your energy level is full at 10. \n Eat or walk to gain energy. \n")
+print("Your energy level is full at 10. \n Eat, walk, or rest to gain energy. \n")
 
 while distance >= 0:  
     choice = input("\nPick a choice, type 'walk' or 'run' \n").lower()
@@ -15,7 +16,7 @@ while distance >= 0:
         print(spacer + "\tDistance from home is {}km\t Your energy level is : {}".format(distance, energy) + spacer)
     elif choice == 'run':
         distance += 5 
-        energy -= 2
+        energy -= 3
         print(spacer + "\tDistance from home is {}km\t Your energy level is : {}".format(distance, energy) + spacer)
         if energy <= 0: 
             print(spacer + "You have no energy, walk to cool off!")
@@ -26,6 +27,10 @@ while distance >= 0:
     elif choice == "eat": 
         energy += 2
         print(spacer + "\tDistance from home is {}km\t Your energy level is : {}".format(distance, energy) + spacer)
+    elif choice == "rest": 
+        energy += 3
+        print(spacer + "\tDistance from home is {}km\t Your energy level is : {}".format(distance, energy) + spacer)
+    
     else: 
         print("Something went wrong")
         print(spacer + "\tDistance from home is {}km\t Your energy level is : {}".format(distance, energy) + spacer)
